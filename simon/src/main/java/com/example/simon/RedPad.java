16bb1e575f0db005f115c398e24a499994bb19f2
Gabe Williams <gcwilliams27101@gmail.com>
2026-04-24 16:39:31 +0000

    Documented redPad

--- simon/src/main/java/com/example/simon/RedPad.java
+++ simon/src/main/java/com/example/simon/RedPad.java
@@ -1,20 +1,33 @@
+/**
+ * Date: 4/24/2026
+ * 
+ * For Simon says color memory game project in class CSC-331
+ * 
+ * PURPOSE: RedPad controls the red pad/button of out memory game which is 1 of 4 
+ * pads. It extends the complete simon pad and controls the sound and color
+ * of specifically the red pad.
+ */
 package application;
 
 import javafx.scene.control.Button;
 
 public class RedPad extends SimonPad {
-
+    // Extends simonPad and controls red color pad which includes sound and color changes
     public RedPad(Button button) {
+        //Uses the attributes from the simonPad super classes
         super(0, button, "-fx-background-color: red;");
     }
 
     @Override
     public String getBrightStyle() {
+        //Changes color of red pad to lighter version when needed within the game
+        //A lighter version of red is pink
         return "-fx-background-color: pink;";
     }
 
     @Override
     public int getMidiNote() {
+        //Pairs a sound with the selection and/or indication of the redPad
         return 69;
     }
 }
