dc2108d19f8c80d124e45801de9cc4a8449958ea
Gabe Williams <gcwilliams27101@gmail.com>
2026-04-25 20:59:16 +0000

    final doc

--- simon/src/main/java/module-info.java
+++ simon/src/main/java/module-info.java
@@ -1,3 +1,11 @@
+/**
+ * Date: 4/24/2026
+ * 
+ * For Simon says color memory game project in class CSC-331
+ * 
+ * Purpose: Model info declares the JavaFX and dependencies 
+ * the Simon game needs and exposes the application package so JavaFX.
+ */
 module com.example.simon {
     requires javafx.controls;
     requires javafx.fxml;
