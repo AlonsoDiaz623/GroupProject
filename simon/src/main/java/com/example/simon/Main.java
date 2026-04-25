53a24051181477127ecf4221734d7404a0ed4642
Gabe Williams <gcwilliams27101@gmail.com>
2026-04-25 18:11:21 +0000

    Main Documented

--- simon/src/main/java/com/example/simon/Main.java
+++ simon/src/main/java/com/example/simon/Main.java
@@ -1,14 +1,25 @@
+/**
+ * Date: 4/25/2026
+ * 
+ * For Simon says color memory game project in class CSC-331
+ * 
+ * Purpose: Launches the JavaFX application and loads the main FXML
+ */
 package application;
 
+//Import JavaFX classes
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
+    //Mian class should extend application
     @Override
     public void start(Stage stage) throws Exception {
+        //Creates an FXML loader and points it to the Simon.fxml file
+        //FXML layout gets wrapped inside a Scene
+        //Lastly sets title,scene, and show
         FXMLLoader loader = new FXMLLoader(getClass().getResource("Simon.fxml"));
         Scene scene = new Scene(loader.load());
         stage.setTitle("Simon Memory Game");
@@ -17,6 +28,7 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
+        //Starts everything
         launch(args);
     }
 }
